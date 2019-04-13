6a25f412e24800b25a7adc00241503e23a64a063
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 22:24:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,12 @@
 class BankAccount
+  
+  attr_reader :name 
+  attr_writer :balance, :status
 
-
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = open 
+  end 
+  
 end
